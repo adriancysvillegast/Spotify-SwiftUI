dcceeadee52398288e77f12e0b0f3a771354e104
Adriancys Villegas <adriancysvillegast@gmail.com>
2023-11-19 11:55:25 -0400

    Initial Commit

---
+++ Spotify/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Spotify
+//
+//  Created by Adriancys Jesus Villegas Toro on 19/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by Adriancys Jesus Villegas Toro on 19/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
