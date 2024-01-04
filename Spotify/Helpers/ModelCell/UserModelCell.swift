08511f6e433c09dd34eb564ddeda79c838aaf051
Adriancys Villegas <adriancysvillegast@gmail.com>
2024-01-04 16:41:54 -0400

    show profile user

---
+++ Spotify/Helpers/ModelCell/UserModelCell.swift
@@ -0,0 +1,15 @@
+//
+//  UserModelCell.swift
+//  Spotify
+//
+//  Created by Adriancys Jesus Villegas Toro on 4/1/24.
+//
+
+import Foundation
+
+struct UserModelCell {
+    let name: String
+    let image: URL?
+    let country: String
+    let email: String
+}
